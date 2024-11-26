9b7eac8f26915b5ecfea9055b52314bdb8ae6c77
M1lksh4k33 <netza.baptichon@stu.ocsb.ca>
2024-11-25 19:18:01 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,5 @@
+# Created by: Netza
+# Created on: 2024/11/25
+# 
+# This program says, "Hello, World!"
+basic.show_string("Hello, World!")
---
+++ main.ts
@@ -0,0 +1,9 @@
+// Created by: Netza
+// Created on: 2024/11/25
+// 
+// This program says, "Hello, World!"
+// Created by: Netza
+// Created on: 2024/11/25
+// 
+// This program says, "Hello, World!"
+basic.showString("Hello, World!")
